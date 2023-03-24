7a64fdbeecd0d447a42ddf8f43540ba8f3e60331
mmanchon <manel.manchon@glovoapp.com>
2023-03-24 18:21:17 +0100

    first commit

---
+++ src/test/java/com/crossfit/alphalink/AlphalinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossfit.alphalink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphalinkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
